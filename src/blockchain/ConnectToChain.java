5d6c8ca1401d6217ad92741696cd2eea0ce9b3a2
WielandF <fabian-wieland@gmx.de>
2018-07-19 10:10:19 +0200

    MultichainJavaAPI

---
+++ src/blockchain/ConnectToChain.java
@@ -0,0 +1,14 @@
+package blockchain;
+
+public class ConnectToChain {
+
+	public ConnectToChain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
